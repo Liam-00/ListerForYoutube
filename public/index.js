25c80144c57942f17ef308baf64c64f21f58f2f3
Liam-00 <93989398+Liam-00@users.noreply.github.com>
2024-04-12 20:40:31 +0200

    add load in animation to videocards

--- public/index.js
+++ public/index.js
@@ -2,9 +2,17 @@
 const VideoCard = ({title, date, thumbnail, id}) => {
     let link = `http://youtube.com/watch?v=${id}`
     
+    const cardRef = React.useRef(null)
+
+    const handleImgLoad = () => {
+        cardRef.current.classList.add("videocard_visible")
+    }
+    
     return (
-        <div className="videocard">
-            <a href={link}><img src={thumbnail}/></a>
+        <div className={`videocard`} ref={cardRef} style={{transition: "opacity 0.5s"}}>
+            <a href={link}>
+                <img src={thumbnail} onLoad={handleImgLoad} />
+            </a>
             <div className="videoinfoblock">
                 <a href={link}> 
                     <h2>{title}</h2>
