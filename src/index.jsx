a1b7b6fe911b6606fa8c40f0a0957df81ddcc10a
Liam-00 <93989398+Liam-00@users.noreply.github.com>
2024-04-25 18:08:30 +0200

    change scroll to top rate

--- src/index.jsx
+++ src/index.jsx
@@ -119,7 +119,7 @@ const App = () => {
     }
     
     const handleScrollToTop = () => {
-        window.scrollTo( {top: 0, left: 0, behavior: "smooth"} )
+        window.scrollTo( {top: 0, left: 0, behavior: "instant"} )
     }
     
     //SCROLL POSITION HANDLING
