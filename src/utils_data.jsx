d370d07cf6c7c69674e85c31653b2ba893a33dac
Liam-00 <93989398+Liam-00@users.noreply.github.com>
2024-05-23 21:08:08 +0200

    create data utils

---
+++ src/utils_data.jsx
@@ -0,0 +1,51 @@
+/*
+CHANNELDATA
+{
+    channeId:string : channelName:string,
+    channeId:string : channelName:string,
+    ...
+}
+
+
+PLAYLISTDATA
+{
+    channelId: [playlists:youtuberesponse]
+}
+
+
+SCROLLDATA
+{
+    channelId: scrollDistance:int
+}
+
+CURRENTCHANNEL
+channeId:string
+*/
+
+export const createChannelData = ( newChannelDataEntry = undefined, oldChannelData = undefined) => {
+    let channelId = newChannelDataEntry ? Object.keys(newChannelDataEntry)[0] : null
+
+    return {
+        ...(oldChannelData ? oldChannelData : {}),
+        ...(newChannelDataEntry ? newChannelDataEntry : {})
+    }
+}
+
+
+export const createPlaylistData = (newChannelPlaylistEntry = undefined, oldPlaylistData = undefined) => {
+    let channelId = newChannelPlaylistEntry ? Object.keys(newChannelPlaylistEntry)[0] : null
+
+    return {
+        ...(oldPlaylistData ? oldPlaylistData : {}),
+        ...(newChannelPlaylistEntry ? newChannelPlaylistEntry : {})
+    }
+}
+
+export const createScrollData = (newScrollDataEntry = undefined, oldScrollData = undefined) => {
+    let channelId = newScrollDataEntry ? Object.keys(newScrollDataEntry)[0] : null
+
+    return {
+        ...(oldScrollData ? oldScrollData : {}),
+        ...(newScrollDataEntry ? newScrollDataEntry : {})
+    }
+}
